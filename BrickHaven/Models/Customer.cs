0e98830cb87e225c124540b81c6880dfdcea5353
jmcguire001 <120243972+jmcguire001@users.noreply.github.com>
2024-04-09 12:00:51 -0600

    Customer class

--- BrickHaven/Models/Customer.cs
+++ BrickHaven/Models/Customer.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
 using System.ComponentModel.DataAnnotations;
 
 namespace BrickHaven.Models
 {
     public class Customer : IdentityUser
     {
+        public int? CustomerId { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
+        public DateTime? Birthday { get; set; }
+        public string? ResidenceCountry { get; set; }
+        public string? Gender { get; set; }
+        public float Age { get; set; }
     }
 }
