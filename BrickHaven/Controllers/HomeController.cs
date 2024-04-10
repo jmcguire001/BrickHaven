4dfde58bd9250939df143fb70f33d584015b81d6
Adam Ross <adamross202@gmail.com>
2024-04-10 15:21:59 -0600

    filter for how many items to display works and maintains state

--- BrickHaven/Controllers/HomeController.cs
+++ BrickHaven/Controllers/HomeController.cs
@@ -31,7 +31,6 @@ namespace BrickHaven.Controllers
         public IActionResult Shop(int pageNum, string? legoType, int pageSize=5) // 'page' means something in dotnet
         {
             // How many items to show per page
-            int pageSize2 = pageSize; // default pageSize is 5
             pageNum = pageNum <= 0 ? 1 : pageNum; // If pageNum is 0, set it to 1
 
             // This variable will hold everything from ProductListViewModel, and then be passed to Index.cshtml
@@ -41,18 +40,19 @@ namespace BrickHaven.Controllers
                 Products = _repo.Products
                     .Where(x => x.Category == legoType || legoType == null) // If legoType is null, show all legos
                     .OrderBy(x => x.Name)
-                    .Skip((pageNum - 1) * pageSize2) // NOT SURE WHAT THIS DOES
-                    .Take(pageSize2), // Only gets a certain number of legos
+                    .Skip((pageNum - 1) * pageSize) // NOT SURE WHAT THIS DOES
+                    .Take(pageSize), // Only gets a certain number of legos
 
                 // This info is for pagination
                 PaginationInfo = new PaginationInfo
                 {
                     CurrentPage = pageNum,
-                    ItemsPerPage = pageSize2,
+                    ItemsPerPage = pageSize,
                     TotalItems = legoType == null ? _repo.Products.Count() : _repo.Products.Where(x => x.Category == legoType).Count() // If legoType is null, show all legos, otherwise, filter specific legos
                 },
 
-                CurrentLegoType = legoType
+                CurrentLegoType = legoType,
+                CurrentPageSize = pageSize
             };
 
             return View(shopInfo);
