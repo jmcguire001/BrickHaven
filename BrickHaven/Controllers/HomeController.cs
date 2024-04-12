1409fd7b6093b9bcb316eb04ab6b848dd6d434fc
Adam Forbush <103288909+aforbush1@users.noreply.github.com>
2024-04-11 20:13:52 -0600

    Commit

--- BrickHaven/Controllers/HomeController.cs
+++ BrickHaven/Controllers/HomeController.cs
@@ -143,7 +143,7 @@ namespace BrickHaven.Controllers
                 return NotFound(); // Return a 404 Not Found response
             }
 
-            var recommendationIds = new List<int?>
+            var item_recommendation_Ids = new List<int?>
             {
                 product.Recommendation1,
                 product.Recommendation2,
@@ -153,7 +153,7 @@ namespace BrickHaven.Controllers
             }.Where(id => id.HasValue).Select(id => id.Value);
 
             var recommendedProducts = await _repo.Products
-                .Where(p => recommendationIds.Contains(p.ProductId))
+                .Where(p => item_recommendation_Ids.Contains(p.ProductId))
                 .ToListAsync();
 
             var viewModel = new ProductRecommendationsViewModel
@@ -181,6 +181,16 @@ namespace BrickHaven.Controllers
             {
                 return View();
             }
+
+        }
+
+        [AllowAnonymous]
+        public IActionResult TEST()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            ViewData["UserId"] = userId; // Pass UserId to the view through ViewData
+                                         // Alternatively, you can pass it as part of a model to the view
+            return View();
         }
     }
 }
